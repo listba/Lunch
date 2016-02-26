152a510e1dbd7bb93521640f78b35d2b3c56e36d
Ben List <Ben.List89@gmail.com>
2016-02-26 16:01:34 -0500

    Fixed Model

--- Lunch/Lunch/Models/TripModel.cs
+++ Lunch/Lunch/Models/TripModel.cs
@@ -34,6 +34,15 @@ namespace Lunch.Models
         public int Votes { get; set; }
     }
 
+    public class RestaurantModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+
     public class TripRestaurantModel
     {
         public int Id { get; set; }
