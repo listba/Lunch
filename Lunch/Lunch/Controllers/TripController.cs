819c98b3d262c52976513cbfa8d4332af79386ac
Ben List <Ben.List89@gmail.com>
2016-02-26 14:52:16 -0500

    Add and create trips

---
+++ Lunch/Lunch/Controllers/TripController.cs
@@ -0,0 +1,34 @@
+﻿using LunchWars.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Lunch.Controllers
+{
+    public class TripController : ApiController
+    {
+
+        public IEnumerable<Trip> GetTrips()
+        {
+            var o = Enumerable.Empty<Trip>();
+            using (var s = new LunchWarsEntities())
+            {
+                o = s.Set<Trip>();
+            }
+            return o;
+        }
+
+        public bool PostTrip(String name)
+        {
+            using (var s = new LunchWarsEntities())
+            {
+                var t = s.Trips.Create();
+                t.Name = name;
+                s.Trips.Add(t);
+                return s.SaveChanges() == 1;
+            }
+        }
+    }
+}
